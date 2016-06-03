2fb18b55dc0d9e5b0a1cfe7e48d128598c05ae05
wusuopubupt <wdxbupt2009@gmail.com>
2016-06-03 15:38:10 +0800

    scala For&While loop

---
+++ src/Loop.scala
@@ -0,0 +1,18 @@
+//Loops using for  
+var sum = 0  
+var i = 0
+for ( i <- 0 until 10) {  
+  sum += i  
+}  
+println(sum)  
+
+sum = 0
+i = 0
+// Loops using while
+while( i < 10) {
+	sum += i
+	i += 1
+}
+println(sum)
+
+
