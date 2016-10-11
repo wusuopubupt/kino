5ebb7f016b7ef5edc08d8edd478bceab87b6e5ce
Wang Dongxu <wangdongxu@4paradigm.com>
2016-10-11 14:40:00 +0800

    java&scala helloworld project

---
+++ create_scala_project.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate \
+	-DarchetypeGroupId=org.scala-tools.archetypes \
+	-DarchetypeArtifactId=scala-archetype-simple  \
+	-DremoteRepositories=http://scala-tools.org/repo-releases \
+	-DgroupId=com.mathandcs\
+	-DartifactId=helloworld \
+	-Dversion=1.0-SNAPSHOT
---
+++ helloworld/src/main/scala/com/mathandcs/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.mathandcs
+
+/**
+ * @author ${user.name}
+ */
+object HelloWorld {
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+  }
+
+}
