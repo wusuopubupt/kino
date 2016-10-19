c144cf422b5f1d83c3c5b6f780ce9fd35cb3fef6
wusuopubupt <wdxbupt2009@gmail.com>
2016-10-19 10:30:08 +0800

    added shape factory

---
+++ agile/src/main/java/com/mathandcs/kino/agile/ShapeFactory.java
@@ -0,0 +1,9 @@
+package com.mathandcs.kino.agile;
+
+/**
+ * Created by wangdongxu on 10/15/16.
+ */
+public interface ShapeFactory {
+    public Shape makeCircle();
+    public Shape makeSquare();
+}
