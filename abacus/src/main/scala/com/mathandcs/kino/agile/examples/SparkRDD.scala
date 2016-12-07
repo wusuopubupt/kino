2bce43f49d596114824d4832cf25ac9928e456a5
wusuopubupt <wdxbupt2009@gmail.com>
2016-12-07 20:02:13 +0800

    multiple output split

--- abacus/src/main/scala/com/mathandcs/kino/agile/examples/SparkRDD.scala
+++ abacus/src/main/scala/com/mathandcs/kino/agile/examples/SparkRDD.scala
@@ -1,6 +1,7 @@
 package com.mathandcs.kino.agile.examples
 
 import com.mathandcs.kino.agile.utils.SparkUtil
+import org.apache.spark.rdd.RDD
 
 /**
   * Created by wangdongxu on 10/11/16.
@@ -9,15 +10,26 @@ object SparkRDD {
   def main(args: Array[String]) {
 
     val sc = SparkUtil.sparkContext
-    val rdd = sc.textFile("src/main/resources/tags.txt")
+    //val rdd = sc.textFile("src/main/resources/tags.txt")
 
-    val lines = rdd.collect()
+    val rdd = sc.parallelize(List(("Tom", 40), ("Jack", 23), ("Tom", 24)), 3)
+    val rdd_map_partitions = rdd.mapPartitionsWithIndex {
+      // 'index' represents the Partition No
+      // 'iterator' to iterate through all elements
+      //                         in the partition
+      (index, iterator) => {
+        println("Called in Partition -> " + index)
+        val myList = iterator.toList
+        // In a normal user case, we will do the
+        // the initialization(ex : initializing database)
+        // before iterating through each element
+        myList.map(x => x + " -> " + index).iterator
+      }
+    }
 
-    val first = rdd.first()
-    println("rdd first: " + rdd.first())
+    println(rdd.partitioner.size)
 
-    val count = rdd.count()
-    println("tag counts: " + count)
 
   }
+
 }
