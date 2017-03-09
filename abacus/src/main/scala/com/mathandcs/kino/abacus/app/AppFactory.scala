961ae357f30152a8a26433002a762a137a03faa7
wusuopubupt <wdxbupt2009@gmail.com>
2017-03-09 11:12:05 +0800

    feat: add app factory

--- abacus/src/main/scala/com/mathandcs/kino/abacus/app/AppFactory.scala
+++ abacus/src/main/scala/com/mathandcs/kino/abacus/app/AppFactory.scala
@@ -1,8 +1,20 @@
 package com.mathandcs.kino.abacus.app
 
 /**
-  * Created by wangdongxu on 3/8/18.
+  * Created by dash wang on 3/8/17.
   */
 object AppFactory {
+
+  def produce(appName: String): BaseApp = {
+    appName match {
+      case "DataStatistics" => new DataStatistics
+      case "DataSplit" => new DataSplit
+      case "HiveSQLExecutor" => new HiveSQLExecutor
+      case "ClassificationModelEval" => new ClassificationModelEval
+      case "RegressionModelEval" => new RegressionModelEval
+      case "TemporalSplit" => new TemporalSplit
+      case _ => throw new IllegalArgumentException(s"Unsupported app name: ${appName}")
+    }
+  }
 
 }
