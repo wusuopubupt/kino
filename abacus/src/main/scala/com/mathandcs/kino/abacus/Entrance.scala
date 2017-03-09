2b8ec007b1db3d0df061dcb7f188a9edd6a7de4a
wusuopubupt <wdxbupt2009@gmail.com>
2017-03-09 11:12:23 +0800

    feat: initialize entrance

--- abacus/src/main/scala/com/mathandcs/kino/abacus/Entrance.scala
+++ abacus/src/main/scala/com/mathandcs/kino/abacus/Entrance.scala
@@ -1,8 +1,18 @@
 package com.mathandcs.kino.abacus
 
+import com.mathandcs.kino.abacus.app.{AppFactory, DataStatistics}
+import org.apache.spark.Logging
+
 /**
-  * Created by wangdongxu on 3/8/18.
+  * Created by dash wang on 5/11/16.
   */
-class Entrance {
+object Entrance extends Logging {
+
+  def main(args: Array[String]) {
+    val appClassName = args(0)
+    val appConfigFile = args(1)
+    val app = AppFactory.produce(appClassName)
+    app.execute(appConfigFile)
+  }
 
 }
