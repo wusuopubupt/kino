98591d5c6f14a473afebcdd14c915082671d05d8
wusuopubupt <wdxbupt2009@gmail.com>
2017-03-12 20:17:48 +0800

    added properties to pom.xml

---
+++ abacus/src/test/scala/com/mathandcs/kino/abacus/ml/evaluation/BinaryClassificationMetricsTest.scala
@@ -0,0 +1,10 @@
+package com.mathandcs.kino.abacus.ml.evaluation
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by wangdongxu on 3/12/17.
+  */
+class BinaryClassificationMetricsTest extends FlatSpec {
+
+}
---
+++ abacus/src/test/scala/com/mathandcs/kino/abacus/ml/util/SparkFunSuite.scala
@@ -0,0 +1,32 @@
+package com.mathandcs.kino.abacus.ml.util
+
+// scalastyle:off
+import org.apache.spark.Logging
+import org.scalatest.{FunSuite, Outcome}
+
+/**
+  * Base abstract class for all unit tests in Spark for handling common functionality.
+  */
+private abstract class SparkFunSuite extends FunSuite with Logging {
+  // scalastyle:on
+
+  /**
+    * Log the suite name and the test name before and after each test.
+    *
+    * Subclasses should never override this method. If they wish to run
+    * custom code before and after each test, they should mix in the
+    * {{org.scalatest.BeforeAndAfter}} trait instead.
+    */
+  final protected override def withFixture(test: NoArgTest): Outcome = {
+    val testName = test.text
+    val suiteName = this.getClass.getName
+    val shortSuiteName = suiteName.replaceAll("org.apache.spark", "o.a.s")
+    try {
+      logInfo(s"\n\n===== TEST OUTPUT FOR $shortSuiteName: '$testName' =====\n")
+      test()
+    } finally {
+      logInfo(s"\n\n===== FINISHED $shortSuiteName: '$testName' =====\n")
+    }
+  }
+
+}
