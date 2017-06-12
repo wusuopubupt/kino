519eb9c153539d102d695dabefc31cef381f2cd6
wusuopubupt <wdxbupt2009@gmail.com>
2017-06-12 18:25:38 +0800

    maven test parallel execution

---
+++ effective-java/src/test/java/com/mathandcs/kino/effectivejava/cache/ParallelTest.java
@@ -0,0 +1,38 @@
+package com.mathandcs.kino.effectivejava.cache;
+
+import org.junit.Test;
+
+/**
+ * Created by wangdongxu on 6/12/17.
+ *
+ * maven并行跑测试
+ * ref: http://memorynotfound.com/run-junit-tests-parallel-using-maven/
+ */
+public class ParallelTest {
+
+    @Test
+    public void one() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+    @Test
+    public void two() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+    @Test
+    public void three() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+    @Test
+    public void four() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+    @Test
+    public void five() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+}
