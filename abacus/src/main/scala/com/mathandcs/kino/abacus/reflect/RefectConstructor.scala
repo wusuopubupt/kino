4155d11af9401f7ecdce6dcd053535812b492f6f
wusuopubupt <wdxbupt2009@gmail.com>
2017-10-12 12:03:55 +0800

    scala reflect get constructor with parameters example

---
+++ abacus/src/main/scala/com/mathandcs/kino/abacus/reflect/RefectConstructor.scala
@@ -0,0 +1,24 @@
+package com.mathandcs.kino.abacus.reflect
+
+import java.sql.Date
+
+/**
+  * Created by dashwang on 10/12/17.
+  */
+object ReflectConstructor {
+
+  // 反射获取带参数的构造器
+  def initClass[T](clazz: java.lang.Class[T])(args:AnyRef*): T = {
+    //val constructor = clazz.getConstructors()(0)
+    val constructor = clazz.getConstructor(classOf[Long])
+    return constructor.newInstance(args:_*).asInstanceOf[T]
+  }
+
+  def main(args: Array[String]): Unit = {
+    val date1 = Date.valueOf("2017-01-01")
+    // 需要AnyRef包装类型,把scala.Long转化为java.lang.Long
+    val arg = java.lang.Long.valueOf(date1.getTime)
+    val date2 = initClass[Date](classOf[Date])(arg)
+    assert(date1.getTime == date2.getTime)
+  }
+}
