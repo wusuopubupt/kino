4fff8450778b71c149ae8ce70895280c866fa1b7
wusuopubupt <wdxbupt2009@gmail.com>
2017-11-22 17:40:36 +0800

    feat: design pattern, Facotry Method

---
+++ agile/src/main/java/com/mathandcs/kino/agile/factoryMethod/Sender.java
@@ -0,0 +1,5 @@
+package com.mathandcs.kino.agile.factoryMethod;
+
+public interface Sender {
+    public void send();
+}
---
+++ agile/src/main/java/com/mathandcs/kino/agile/factoryMethod/SenderFactory.java
@@ -0,0 +1,17 @@
+package com.mathandcs.kino.agile.factoryMethod;
+
+/**
+ * Created by dashwang on 11/22/17.
+ */
+public class SenderFactory {
+    public Sender produce(String type) {
+		if("mail".equals(type)) {
+			return new MailSender();
+		} else if("sms".equals(type)) {
+			return new SmsSender();
+		} else {
+			System.out.println("Unknown type: " + type);
+			return null;
+		}	
+    }
+}
