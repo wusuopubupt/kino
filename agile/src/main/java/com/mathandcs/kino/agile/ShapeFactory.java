6bd316a245ed0a1a6a79704f985fdf0ff56d002a
wusuopubupt <wdxbupt2009@gmail.com>
2017-11-23 20:06:14 +0800

    feat: add decorator design pattern UML photo

--- agile/src/main/java/com/mathandcs/kino/agile/ShapeFactory.java
+++
@@ -1,9 +0,0 @@
-package com.mathandcs.kino.agile;
-
-/**
- * Created by dashwang on 10/15/16.
- */
-public interface ShapeFactory {
-    public Shape makeCircle();
-    public Shape makeSquare();
-}
