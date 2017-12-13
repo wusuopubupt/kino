52ebe4230c8234c487b78234858496d8819721f5
wusuopubupt <wdxbupt2009@gmail.com>
2017-12-13 15:35:09 +0800

    feat: add jython example

--- effective-java/src/main/java/com/mathandcs/kino/effectivejava/compiler/jpython/PythonScriptEvaluator.java
+++ effective-java/src/main/java/com/mathandcs/kino/effectivejava/compiler/jpython/PythonScriptEvaluator.java
@@ -4,20 +4,42 @@ import org.python.core.PyFunction;
 import org.python.core.PyInteger;
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
+import java.nio.file.Paths;
 
 /**
  * Created by dashwang on 12/12/17.
  */
 public class PythonScriptEvaluator {
 
+    private static PythonInterpreter interpreter = new PythonInterpreter();
 
+    private static final String PYTHON_UTIL_SCRIPT_PATH = "effective-java/src/main/resources/python/my_util.py";
+    private static final String PYTHON_MAIN_SCRIPT_PATH = "effective-java/src/main/resources/python/main.py";
+    private static final String PYTHON_UTIL_SCRIPT_RELATIVE_CLASSPATH = "effective-java/src/main/resources/python";
+
+    private static void callPythonFunction() {
+        PyFunction func = (PyFunction)interpreter.get("add",PyFunction.class);
+        int a = 1, b = 2 ;
+        PyObject pyobj = func.__call__(new PyInteger(a), new PyInteger(b));
+        System.out.println("answer = " + (long) pyobj.__tojava__(Long.class));
+    }
+
+    // http://blog.csdn.net/xfei365/article/details/50996727, solved jython "ImportError: No module named" error
+    private static void reloadClassPath() {
+        String curDir = Paths.get(".").toAbsolutePath().normalize().toString();
+        interpreter.exec("import sys");
+        interpreter.exec(String.format("sys.path.append('%s')", Paths.get(curDir, PYTHON_UTIL_SCRIPT_RELATIVE_CLASSPATH)));
+        interpreter.exec("print(sys.path)");
+    }
 
     public static void main(String args[]){
-        PythonInterpreter interpreter = new PythonInterpreter();
-        interpreter.execfile("C:\\Python27\\programs\\my_utils.py");
-        PyFunction func = (PyFunction)interpreter.get("adder",PyFunction.class);
-        int a = 2010, b = 2 ;
-        PyObject pyobj = func.__call__(new PyInteger(a), new PyInteger(b));
-        System.out.println("anwser = " + pyobj.toString());
+        reloadClassPath();
+
+        // call python function
+        interpreter.execfile(PYTHON_UTIL_SCRIPT_PATH);
+        callPythonFunction();
+
+        // execute python file directly
+        interpreter.execfile(PYTHON_MAIN_SCRIPT_PATH);
     }
 }
--- effective-java/src/main/resources/python/main.py
+++ effective-java/src/main/resources/python/main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
-def add(a, b):
-    return a + b
+import my_util
+
+def main():
+    sum = my_util.add(1, 2)
+    print("sum of 1 and 2 is : %d" % sum)
+
+
+if __name__ == '__main__':
+    main()
