9709f729d82738fa7273e3909f0a2122294535d4
wusuopubupt <wdxbupt2009@gmail.com>
2018-03-09 14:13:50 +0800

    test: add unit test for Entrance

---
+++ abacus/src/test/scala/com/mathandcs/kino/abacus/EntranceTest.scala
@@ -0,0 +1,17 @@
+package com.mathandcs.kino.abacus
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by dash wang on 3/9/17.
+  */
+class EntranceTest extends FlatSpec {
+
+  behavior of "EntranceTest"
+
+  it should "main" in {
+    val args = Array("DataSplit","src/test/resources/data-split.json")
+    Entrance.main(args)
+  }
+
+}
